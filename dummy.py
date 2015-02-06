6f10d506bf36572b53f0325fef6dc8a1bac5f4fd
kppatel <kppatel@verisign.com>
2015-02-06 15:56:16 +0530

    added more crappy codes

--- dummy.py
+++ dummy.py
@@ -4,7 +4,11 @@ def foo():
 	"""
 	This is crappy function. should be removed using git checkout
 	"""
-	return None
+	if True == True:
+		return True
+	else:
+		return False
+
 
 def main():
 	pass
